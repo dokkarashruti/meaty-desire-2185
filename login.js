50d82778e9cfc0a824a8cb8a7c930cf729c4b740
dokkarashruti <dokkarashruti41@gmail.com>
2022-08-28 14:18:20 +0530

    day-5

--- login.js
+++ login.js
@@ -10,11 +10,11 @@ function sidelogin() {
   </div>
   <div id="logininput">
     <label for="">Email address <span>*</span></label>
-    <input type="email" id="mail" />
+    <input type="email" id="mail" placeholder="enter email here" />
     <label for="">Password <span>*</span></label>
-    <input type="password" id="password">
+    <input type="password" id="pass" placeholder="enter password here">
     <a href="">Forgot your password?</a>
-    <button>LOGIN</button>
+    <button onclick="log()">LOGIN</button>
   </div>
   <div class="fullbtn" id="facebook">
     <div>
@@ -30,14 +30,14 @@ function sidelogin() {
   </div>
   <div id="noaccount">
     <i class="fa-regular fa-user"></i>
+    <input type="text" id="name" placeholder="enter name here">
+    <input type="text" id="email" placeholder="enter email here">
+    <input type="password" id="Password" placeholder="enter password here">
     <h4>No account yet?</h4>
     <p>Welcome to Kimaye! Help us with a few details to know you better as you take the journey of enjoying our safe and tasty fruits.</p>
     <button onclick="sgnup()">Create An Account</button>
   </div>
 </div>`;
-}
-function sidesignup(){
-  return ``
 }
 let login = document.getElementById("login");
 let show = document.getElementById("sidelogin");
@@ -50,9 +50,47 @@ function user() {
 function cls() {
   show.innerHTML = null;
 }
-
+let signup = document.getElementById("signup");
 function sgnup() {
-  show.innerHTML = null;
-  show.innerHTML = sidelogin();
-  
+  let name=document.getElementById("name").value
+  let email=document.getElementById("email").value
+  let password=document.getElementById("Password").value
+  let obj={
+    name:name,
+    email:email,
+    password:password,
+  };
+  console.log(obj)
+  if(!name || !email || !password){
+    alert("fill all the inputs")
+  }else{
+    let obj={
+      name,email,password
+    }
+  }
+  localStorage.setItem("sruthi",JSON.stringify(obj))
+  document.getElementById("name").value=""
+  document.getElementById("email").value=""
+  document.getElementById("password").value=""
+  alert("sign up succesful")
+  window.location.href="#logininput"
+}
+
+
+let data=JSON.parse(localStorage.getItem("sruthi"))||[]
+
+function log(){
+  let email=document.getElementById("mail").value;
+  let password=document.getElementById("pass").value;
+  if(!email|| !password){
+    alert("fill all inputs")
+  }else{
+    if(data.email===email && data.password===password){
+        alert("Login Succesful")
+        window.location.href="https://kimaye.com/account/login"
+
+    }else{
+      alert('Wrong Creditials')
+    }
+  }
 }
